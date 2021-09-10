da26ee2f7f15be536e9ef5e096c4ae22a16c1541
jin <jin@localhost.localdomain>
2021-09-10 16:18:18 +0900

    정리

--- models/__init__.py
+++ models/__init__.py
@@ -1,19 +1,3 @@
 '''
-RDB ORM model 과 database 세션 관리 layer
+data, api model layer
 '''
-
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
-
-#SQLALCHEMY_DATABASE_URL = "mysql+mysqlconnector://root:future_01@localhost:3306/test"
-SQLALCHEMY_DATABASE_URL = "sqlite:///./sql.db"
-
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
-
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-Base = declarative_base()
-
-Base.metadata.create_all(bind=engine)
